8f4d4b1f618a51fff80729a74c1dc70a19899c45
Sergii Podobrii <podobri@ukr.net>
2023-10-12 14:59:48 +0300

    add link

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(window).on("load", function () {
-  $("#header").vide("../video/cover.mp4", {
-    bgColor: "#c5a919",
-  });
+  $("#header").vide(
+    "https://drive.google.com/file/d/1ZO19O1sgItCndTidL2SRgAyrJxbtCpb1/view?usp=sharing",
+    {
+      bgColor: "#c5a919",
+    }
+  );
 });
