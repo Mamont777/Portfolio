5b53bf57c31b0dd1be4cde37e62fafdf929309ec
Sergii Podobrii <podobri@ukr.net>
2023-10-12 16:56:55 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(window).on("load", function () {
   $("#header").vide("https://www.youtube.com/watch?v=BT-L55R521k", {
     bgColor: "#c5a919",
+    autoplay: true,
   });
 });
-
-// https://www.youtube.com/watch?v=BT-L55R521k
